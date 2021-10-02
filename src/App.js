426df393f913030fb2de8145894dc5f18d2b7442
webdeveloper104433 <86479945+webdeveloper104433@users.noreply.github.com>
2021-10-02 18:42:53 +0300

    feat: add layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="sm:container sm:mx-auto">
+      <div className="flex h-full">
+        <div className="w-1/5 h-screen bg-purple-100 text-2xl">
+          <div className="mt-10">
+            <a className="font-medium text-blue-500 underline hover:text-blue-700 block" href="#">Take Picture</a>
+            <a className="text-blue-500 hover:text-blue-800 block" href="#">Show all pictures</a>
+          </div>
+        </div>
+        
+        <div className="w-4/5">
+          <div className="m-5 text-blue-500 text-4xl">Take Picture Page</div>
+        </div>
+      </div>
     </div>
   );
 }
